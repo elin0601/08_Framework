2ce942064f810ce8a52cb479a3f4eed54a48a5ae
dlwltn <03dreams@naver.com>
2024-03-28 13:42:02 +0900

    240328_로그인 시도시 발생되는  것들 처리 하는 중 ....

--- boardProject1/src/main/java/edu/kh/project/member/controller/MemberController.java
+++ boardProject1/src/main/java/edu/kh/project/member/controller/MemberController.java
@@ -3,13 +3,18 @@ package edu.kh.project.member.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.kh.project.member.model.dto.Member;
 import edu.kh.project.member.model.service.MemberService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 /* @SessionAttributes( {"key", "key", ....} )
@@ -48,10 +53,22 @@ public class MemberController {
 	 * 
 	 * @param model : 데이터 전달 용 객체(request scope) 
 	 * 
+	 * @param saveId : 아이디 저장 체크 여부
+	 * 
+	 * @param resp : 쿠키 생성, 추가를 위해 얻어온 객체
+	 * 
 	 * @return "redirect:/"
 	 */
 	@PostMapping("login")
-	public String login(Member inputMember, RedirectAttributes ra, Model model) {
+	public String login(Member inputMember, RedirectAttributes ra, Model model, 
+			@RequestParam(value="saveId", required=false) String saveId, 
+			HttpServletResponse resp) {
+		
+		
+		// 체크박스에 value 가 없을 때
+		// 체크가 된 경우 : "on" (null 아님)
+		// 체크가 안된 경우 : null
+		log.debug("saveId", saveId);
 		
 		// 로그인 서비스 호출
 		Member loginMember = service.login(inputMember);
@@ -71,10 +88,71 @@ public class MemberController {
 			
 			// 2단계 : 클래스 위에 @SessionAttributes() 어노테이션 때문에
 			// 		   session scope에 이동됨
+			
+			
+			/* ************************************************************ */
+			// 아이디 저장(Cookie)
+			
+			// 쿠키 객체 생성(K:V)
+			Cookie cookie = new Cookie("saveId", loginMember.getMemberEmail());
+			
+			// 클라이언ㅌ가 어떤 요청을 할 때 쿠키가 첨부될지 지정
+			
+			// ex) "/" : IP 또는 도메인 또는 localhost
+			// 			 뒤에 "/" -> 메인페이지 + 그 하위 주소들
+			cookie.setPath("/"); 
+			
+			// 만료 기간 지정
+			if(saveId != null) { // 아이디 저장 체크 시
+				cookie.setMaxAge(30 * 24 * 60 * 60);  // 30일 (초 단위로 지정)
+				
+			} else { // 미체크시
+				cookie.setMaxAge(0); // 0초 (클라이언트 쿠키를 삭제)
+			}
+			
+			// 응답 객체에 쿠키 추가 -> 클라이언트로 전달
+			resp.addCookie(cookie);
+			
+			/* ************************************************************ */
 		}
 		
 		
 		return "redirect:/"; // 메인 페이지 재요청
 	}
 	
+	
+	/** 로그아웃 : Session에 저장된 로그인된 회원 정보를 없앰(만료, 무효화, 완료)
+	 * 
+	 * @parama SessionStatus : 세션을 완료(없앰) 시키는 역할의 객체
+	 * 		- @SessionAtrributes 로 등록된 세션을 완료
+	 * 		- 서버에서 기존 세션이 사라짐과 동시에 
+	 * 		  새로운 세션 객체가 생성되어 클라이언트와 연결 
+	 * 
+	 * @return "redirect:/"
+	 */
+	@GetMapping("logout")
+	public String logout(SessionStatus status) {
+		status.setComplete(); // session을 완료 시킨(없앰)		
+		return "redirect:/";
+	}
+	
+	
 }
+
+
+/* Cookie란?
+ *  - 클라이언트 측(브라우저)에서 관리하는 데이터(파일 형식)
+ *  
+ *  - Cookie에는 만료기간, 데이터(key=value), 사용하는 사이트(주소)가
+ *    기록되어 있음
+ *    
+ * - 클라이언트가 쿠키에 기록된 사이트로 요청을 보낼 때
+ *   요청에 쿠키가 담겨져서 서버로 넘어감
+ *   
+ * - Cookie의 생성, 수정, 삭제는 Server가 관리
+ *   저장은  Client가 함
+ * 
+ * - Cookie는 HttpServletResponse를 이용해서 생성,
+ *   클라이언트에게 전달(응답) 할 수 있다
+ * */
+
