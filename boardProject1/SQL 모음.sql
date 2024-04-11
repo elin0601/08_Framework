4b3a3a217b2aca1ba46d2e4a4cdaf5434bcf2ccb
dlwltn <03dreams@naver.com>
2024-04-11 12:51:52 +0900

    240411_SQL문 + BOARD DTO 생성

--- boardProject1/SQL 모음.sql
+++ boardProject1/SQL 모음.sql
@@ -558,10 +558,165 @@ COMMIT;
 
 SELECT * FROM "BOARD_TYPE";
 
+---------------------------------------------------------
+
 ----- 게시판 종류 조회(표기법 변경) ------
 SELECT BOARD_CODE "boardCode", BOARD_NAME "boardName" 
 FROM "BOARD_TYPE"
 ORDER BY BOARD_CODE;
+
+
+---------------------------------------------------------
+/* 게시글 번호 시뭔스 생성 */
+CREATE SEQUENCE SEQ_BOARD_NO NOCACHE;
+
+
+/* 게시판(BOARD) 테이블 샘플 데이터 삽입(PL/SQL) */
+
+-- DBMS_RANDOM.VALUE(0, 3) : (0.0 이상, 3.0 미만의 난수) 1,2,3 중 하나
+
+SELECT * FROM "MEMBER";
+
+BEGIN
+	FOR I IN 1..2000 LOOP
+		
+		INSERT INTO "BOARD"
+		VALUES (SEQ_BOARD_NO.NEXTVAL, 
+						SEQ_BOARD_NO.CURRVAL || '번째 게시글',
+						SEQ_BOARD_NO.CURRVAL || '번째 게시글 내용 입니다',
+						DEFAULT, DEFAULT, DEFAULT, DEFAULT,
+						CEIL( DBMS_RANDOM.VALUE(0, 3) ), 
+						6
+		);
+	END LOOP;
+END;
+
+
+-- 게시판 종류별 샘플 데이터 삽입 확인
+SELECT BOARD_CODE , COUNT(*) 
+FROM "BOARD"
+GROUP BY BOARD_CODE 
+ORDER BY BOARD_CODE;
+
+
+------------------------------------------------------------
+-- 부모 댓글 번호 NULL 허옹
+ALTER TABLE "COMMENT" 
+MODIFY PARENT_COMMENT_NO NUMBER NULL;
+
+
+/* 댓글 번호 시퀀스 생성 */
+CREATE SEQUENCE SEQ_COMMENT_NO NOCACHE;
+
+/* 댓글 ("COMMENT") 테이블에 샘플 데이터 추가 */
+BEGIN
+	FOR I IN 1..2000 LOOP
+		
+		INSERT INTO "COMMENT"
+		VALUES (SEQ_COMMENT_NO.NEXTVAL,
+						SEQ_COMMENT_NO.CURRVAL || '번째 댓글 입니다',
+						DEFAULT, DEFAULT,
+						CEIL ( DBMS_RANDOM.VALUE(0, 2000) ),
+						9,
+						NULL -- 부모 댓글 번호
+		);
+	END LOOP;
+END;
+
+
+COMMIT;
+
+-- 게시글 번호 최소값, 최대값
+SELECT MIN(BOARD_NO), MAX(BOARD_NO)  
+FROM "BOARD";
+
+-- 댓글 삽입 확인
+SELECT  BOARD_NO , COUNT(*)
+FROM "COMMENT"
+GROUP BY BOARD_NO 
+ORDER BY BOARD_NO;
+
+SELECT MIN(BOARD_NO), MAX(BOARD_NO)  
+FROM "COMMENT";
+
+
+---------------------------------------------------------
+-- 컬럼명 수정
+ALTER TABLE "BOARD" RENAME COLUMN MEMEBR_NO TO MEMBER_NO;
+
+/* 특정 게시판(BOARD_CODE)에 
+ * 삭제되지 않은 게시글 목록 조회
+ * 
+ * 단, 최신글이 제일 위에 존재
+ * 몇 초/분/시간 전 또는 YYYY-MM-DD 형식으로 작성일 조회
+ * 
+ * + 댓글 갸수
+ * + 좋아요 개수
+ *  */
+
+-- 번호 / 제목[댓글개수] / 작성자닉네임 / 작성일 / 조회수 / 좋아요개수 
+
+
+-- 상관 서브 쿼리 
+-- 1) 메인 쿼리 1행 조회
+-- 2) 1행 조회 결과를 이용해서 서브쿼리 수행
+--    (메인쿼리 모두 조회할 때 까지 반복)
+
+--------------------------------------------------------------------------
+SELECT BOARD_NO , BOARD_TITLE , MEMBER_NICKNAME , READ_COUNT ,
+	(SELECT COUNT(*) 
+	 FROM "COMMENT" C
+	 WHERE C.BOARD_NO = B.BOARD_NO) COMMENT_COUNT,
+	
+	 (SELECT COUNT(*) 
+	  FROM "BOARD_LIKE" L
+	  WHERE L.BOARD_NO = B.BOARD_NO) LIKE_COUNT,
+	  
+	 CASE  
+	 	WHEN SYSDATE - BOARD_WRITE_DATE < 1 / 24 / 60 
+	 	THEN FLOOR((SYSDATE - BOARD_WRITE_DATE) * 24 * 60 * 60) || '초 전'
+	 	 
+	 	WHEN SYSDATE - BOARD_WRITE_DATE < 1 / 24
+	 	THEN FLOOR((SYSDATE - BOARD_WRITE_DATE) * 24 * 60) || '분 전'
+	 	 
+	 	WHEN SYSDATE - BOARD_WRITE_DATE < 1
+	 	THEN FLOOR((SYSDATE - BOARD_WRITE_DATE) * 24) || '시간 전'
+	 	 
+	 	ELSE TO_CHAR(BOARD_WRITE_DATE, 'YYYY-MM-DD') -- 작성한지 하루 지났을 경우 작성일을 나타냄
+	 	
+	 END BOARD_WRITE_DATE
+	 
+FROM "BOARD" B
+JOIN "MEMBER" USING (MEMBER_NO)
+WHERE BOARD_DEL_FL = 'N'
+ORDER BY BOARD_NO DESC;
+
+--------------------------------------------------------------------------
+
+-- 특정 게시글의 댓글 개수 조회
+SELECT COUNT(*) FROM "COMMENT"
+WHERE BOARD_NO = 1999; 
+
+
+-- 현재시간 - 하루 전 --> 정수 부분 == 일 단위
+SELECT (SYSDATE - TO_DATE('2024-04-10 12:14:30', 'YYYY-MM-DD HH24:MI:SS')) * 60 * 60 * 24
+FROM DUAL;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- boardProject1/src/main/java/edu/kh/project/board/controller/BoardController.java
+++ boardProject1/src/main/java/edu/kh/project/board/controller/BoardController.java
@@ -1,6 +1,8 @@
 package edu.kh.project.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import edu.kh.project.board.model.service.BoardService;
@@ -14,5 +16,13 @@ import lombok.extern.slf4j.Slf4j;
 public class BoardController {
 	
 	private final BoardService service;
-
+	
+	@GetMapping("{boardCode}")
+	public String selectBoardList(
+			@PathVariable("boardCode") int boardCode) {
+		
+		// 주소에 적힌 변수명과 일치하는 값을 가져와 변수에 담아서 사용
+		
+		return null;
+	}
 }
