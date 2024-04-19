4ed91d635b69428ceb27c5dec636ed82f201870d
dlwltn <03dreams@naver.com>
2024-04-19 10:59:51 +0900

    240419_통합구현 시험

---
+++ TonghabTest1/src/main/java/com/kh/test/board/controller/BoardController.java
@@ -0,0 +1,46 @@
+package com.kh.test.board.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.test.board.model.dto.Board;
+import com.kh.test.board.model.service.BoardService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/")
+public class BoardController {
+
+	private final BoardService service;
+
+	@GetMapping("search")
+	public String search() {
+		return "/";
+	}
+
+	@PostMapping("search")
+	public String search(Board board, Model model) {
+
+		List<Board> boardSearch = service.search(board);
+
+		String path = null;
+
+		if (!boardSearch.isEmpty())
+			path = "/searchSuccess";
+		else
+			path = "/searchFail";
+		
+		model.addAttribute("boardSearch", boardSearch);
+
+		return path;
+
+	}
+
+}
---
+++ TonghabTest1/src/main/java/com/kh/test/board/model/dto/User.java
@@ -0,0 +1,23 @@
+package com.kh.test.board.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Slf4j
+@ToString
+public class User {
+
+	private int userNo;
+	private String userId;
+	private String userName;
+	private int userAge;
+	
+}
