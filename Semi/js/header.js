432399733bc7ac7d24b88e555da255a9418acbae
DESKTOP-3GBEFFK\DREAMS03 <03dreams@naver.com>
2024-04-22 23:17:29 +0900

    240422_js 안됨

--- Semi/js/header.js
+++ Semi/js/header.js
@@ -1,6 +1,16 @@
 const popupClose = document.querySelector(".popupClose");
 const popupLayer = document.querySelector(".popupLayer");
+const loginForm = document.querySelector("#loginForm")
 
-popupClose.addEventListener("click", () => {
-    popupLayer.classList.add("popup-hidden")
-});
+if(loginForm != null) {
+    popupClose.addEventListener("click", () => {
+        popupLayer.classList.add("popup-hidden");
+    });
+}
+
+const loginIcon = document.querySelector("#loginIcon")
+if(loginIcon != null) {
+    loginIcon.addEventListener("click", ()=>{
+        popupLayer.classList.remove("popup-hidden");
+    });
+}
