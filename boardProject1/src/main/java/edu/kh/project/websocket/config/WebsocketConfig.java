9b78c911bbd6247eb4a8c14cffec58b23824209d
dlwltn <03dreams@naver.com>
2024-05-13 14:26:06 +0900

    240413_실시간 채팅

--- boardProject1/src/main/java/edu/kh/project/websocket/config/WebsocketConfig.java
+++ boardProject1/src/main/java/edu/kh/project/websocket/config/WebsocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
+import edu.kh.project.websocket.handler.ChattingWebsocketHandler;
 import edu.kh.project.websocket.handler.NotificationWebsocketHandler;
 import edu.kh.project.websocket.handler.TestWebsocketHandler;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +24,9 @@ public class WebsocketConfig implements WebSocketConfigurer{
 	
 	// 알림 웹소켓 처리 객체 의존성 주입(DI)
 	private final NotificationWebsocketHandler notificationWebsocketHandler;
+	
+	// 채팅 웹소켓 처리 객체 의존성 주입(DI)
+	private final ChattingWebsocketHandler chattingWebsocketHandler;
 
 	
 	// 웹소켓 핸들러를 등록하는 메서드
@@ -59,10 +63,15 @@ public class WebsocketConfig implements WebSocketConfigurer{
 			.withSockJS();
 		
 		
+		// 채팅 처리하는 핸들러와 주소 연결
+		registry
+		.addHandler(chattingWebsocketHandler, "/chattingSock")
+		.addInterceptors(handshakeInterceptor)
+		.setAllowedOriginPatterns("http://localhost/", 
+				"http://127.0.0.1/", 
+				"http://192.168.10.31/")
+		.withSockJS();
 		
 	}
-	
-	
-	
 
 }
